9ed7a2c9bfaf5e161fb0aefcd24f4ec26d2fd0f7
Pierre-Antoine <pierre-antoine.burdet@hardis.fr>
2023-01-15 15:01:06 +0100

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
